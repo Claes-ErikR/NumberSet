447eaee7aa47ccc69a48b1b7a9f3562c3c52d366
Claes-Erik Rydberg <utte.rydberg@gmail.com>
2023-02-08 14:50:10 +0100

    Added empty NumberSet class

---
+++ NumberSet/NumberSet.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSet
+{
+    public class NumberSet<T> : INumberSet<T>
+    {
+        public T LowerBound => throw new NotImplementedException();
+
+        public T UpperBound => throw new NotImplementedException();
+
+        public bool IsClosed => throw new NotImplementedException();
+
+        public bool IsOpen => throw new NotImplementedException();
+
+        public bool IsEmpty => throw new NotImplementedException();
+
+        public T Measure => throw new NotImplementedException();
+
+        INumberSet<T> Utte.NumberSet.IBoundedSet<T>.Difference(INumberSet<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        INumberSet<T> Utte.NumberSet.IBoundedSet<T>.Difference(INumberSetElement<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        INumberSet<T> Utte.NumberSet.IBoundedSet<T>.Intersection(INumberSet<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        INumberSet<T> Utte.NumberSet.IBoundedSet<T>.Intersection(INumberSetElement<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        INumberSet<T> Utte.NumberSet.IBoundedSet<T>.SymmetricDifference(INumberSet<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        INumberSet<T> Utte.NumberSet.IBoundedSet<T>.SymmetricDifference(INumberSetElement<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        INumberSet<T> Utte.NumberSet.IBoundedSet<T>.Union(INumberSet<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        INumberSet<T> Utte.NumberSet.IBoundedSet<T>.Union(INumberSetElement<T> other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
