ff710ab249fd2494c19b4fc63093af96206196ad
Claes-Erik Rydberg <utte.rydberg@gmail.com>
2023-02-16 16:47:56 +0100

    INumberSetElement and INumberSet comments

--- NumberSet/INumberSet.cs
+++ NumberSet/INumberSet.cs
@@ -3,8 +3,13 @@ using Utte.NumberSet;
 
 namespace NumberSet
 {
+    /// <summary>
+    /// Interface for constructing sets of numberlike objects of type T. It is built of a collection of INumberSetElement and is immutable.
+    /// Every INumberSetElement in the collection should be disjunct and sorted in order.
+    /// T has to implement IAdditionOperators<T, T, T>, ISubtractionOperators<T, T, T>, IComparisonOperators<T, T, bool>
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface INumberSet<T> : IBoundedSet<T>, IReadOnlyList<INumberSetElement<T>> where T : IAdditionOperators<T, T, T>, ISubtractionOperators<T, T, T>, IComparisonOperators<T, T, bool>
     {
-
     }
 }
--- NumberSet/INumberSetElement.cs
+++ NumberSet/INumberSetElement.cs
@@ -3,9 +3,24 @@ using Utte.NumberSet;
 
 namespace NumberSet
 {
+
+    /// <summary>
+    /// Interface for constructing elements of a set which is a set in itself. The interface is used as part of INumberSet
+    /// T has to implement IAdditionOperators<T, T, T>, ISubtractionOperators<T, T, T>, IComparisonOperators<T, T, bool>
+    /// An open INumberSetElement have IncludeLowerBound = false and IncludeUpperBound = false
+    /// An open INumberSetElement have IncludeLowerBound = true and IncludeUpperBound = true
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface INumberSetElement<T> : IBoundedSet<T> where T : IAdditionOperators<T, T, T>, ISubtractionOperators<T, T, T>, IComparisonOperators<T, T, bool>
     {
+        /// <summary>
+        /// Indicates if the lower bound is part of the INumberSetElement
+        /// </summary>
         bool IncludeLowerBound { get; }
+
+        /// <summary>
+        /// Indicates if the upper bound is part of the INumberSetElement
+        /// </summary>
         bool IncludeUpperBound { get; }
     }
 }
